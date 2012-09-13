f1c7c4b0737d22a11105f48694ca7d64da2f9334
David Arango <arango@gmail.com>
2012-09-13 21:09:07 +0200

    First commit, project model

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+Smokr::Application.config.secret_token = '4375ee7f2c620c5f059ae8869c440a68f6c7c873170e19a7feaacf5b675d0778085c6bf3b1f8c36cdeef351aee8f1682d11eed517851b3edd5e874f4f18a662d'
